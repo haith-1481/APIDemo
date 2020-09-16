6205a0f9893b278f35258267095bbae9eb503ed5
haith <trinh.hoang.hai@sun-asterisk.com>
2020-09-16 14:42:50 +0700

    Initial Commit

---
+++ APIDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  APIDemo
+//
+//  Created by trinh.hoang.hai on 9/16/20.
+//  Copyright © 2020 trinh.hoang.hai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
